db3afaf50fb2b5aaa79dd27e541f801121420ec0
Andre Fonseca <decobf15@gmail.com>
2025-03-07 11:24:28 -0300

    exercício feito

---
+++ mod9/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int num1 = 9;
+        System.out.println(num1);
+        Integer num2 = 12;
+
+        Integer numI = (int) num2;
+
+        System.out.println(numI);
+
+        
+
+        
+
+    }
+}
